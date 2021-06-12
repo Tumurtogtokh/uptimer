bc30a70c26a5d9f6816f38aa9c61cc47c48c10d5
tumurtogtokh <tumurtogtokh@gmail.com>
2021-06-12 17:59:21 +0100

    functionality done

--- uptimer/uptimer.py
+++ uptimer/uptimer.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import requests
 import click
 
@@ -12,11 +14,17 @@ def check_url(url):
 
 
 @click.command()
-@click.argument("url")
-def check(url):
-    status_code = check_url(url)
-    if status_code:
-        colorize_status(url, status_code)
+@click.argument("urls", nargs=-1)
+@click.option("--daemon", "-d", default=False, is_flag=True)
+def check(urls, daemon):
+    while True:
+        for url in urls:
+            status_code = check_url(url)
+            if status_code:
+                colorize_status(url, status_code)
+        if not daemon:
+            break
+        sleep(3)
 
 
 def colorize_status(url, status):
@@ -24,10 +32,9 @@ def colorize_status(url, status):
         2: "green",
         3: "yellow",
         4: "bright_red",
-        5: "red",
-        -1: "magenta"
+        5: "red"
     }
-    click.secho(f"{url} --> {status}", fg=colours.get(status // 100, -1))
+    click.secho(f"{url} --> {status}", fg=colours.get(status // 100, "magenta"))
 
 
 if __name__ == "__main__":
